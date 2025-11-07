ce266a5b66c35d1298c54780b9357103072b4aa8
Lewis <136109494+Lewiz1909@users.noreply.github.com>
2025-11-07 14:05:42 +0700

    Update main.cpp (full CRUD)

--- src/main.cpp
+++ src/main.cpp
@@ -2,20 +2,21 @@
 #include<nlohmann/json.hpp>
 #include"config_manager.hpp"
 #include<unordered_set>
-#include<ctime>
+#include<chrono>
 #include<iostream>
 #include<httplib.h>
 #include<fstream>
 using json = nlohmann::json;
 int main() {
-        std::unordered_set<int> Submission;
-        std::unordered_set<int> Report;
+    std::unordered_set<int> Submission; 
+    std::unordered_set<int> Report;
     httplib::Server server;
-server.Get(R"(/(\d+)/reports)", [&](const httplib::Request& req, httplib::Response& res){
+    /* INITIALIZATION */
+    /* GET */
+
+    server.Get(R"(/(\d+)/reports)", [&](const httplib::Request& req, httplib::Response& res){
     int submission_id = std::stoi(req.matches[1]);
-
     std::string path = "data/" + std::to_string(submission_id) + ".json";
-
     std::ifstream f(path);
     if(!f.is_open()){
         res.status = 404;
@@ -33,6 +34,7 @@ server.Get(R"(/(\d+)/reports)", [&](const httplib::Request& req, httplib::Respon
     res.set_content(response.dump(4), "application/json");
 });
 
+    /*POST*/
 
     server.Post(R"(/(\d+)/reports)", [&](const httplib::Request& req, httplib::Response& res){
         json body;
@@ -49,14 +51,19 @@ server.Get(R"(/(\d+)/reports)", [&](const httplib::Request& req, httplib::Respon
         if (!body.contains("report_id") || body["report_id"] == std::string::npos || !body.contains("link")) {
             res.status = 404;
             res.set_content("report_id + link required", "text/plain");
-        }
+        } // update time
+            auto now = std::chrono::system_clock::now();
+            std::time_t t = std::chrono::system_clock::to_time_t(now);
+            std::string date = std::ctime(&t);
+        //
         std::string PATH = "data/" + std::to_string(submission_id) + ".json";
         json submission;
         std::ifstream file(PATH);
         if (!file.is_open()) {
             submission = {
             {"submission_id", submission_id},
-            {"reports", json::array()}
+            {"reports", json::array()}, 
+            {"edited_at", json::array()}
             };
         }  
         else {
@@ -64,15 +71,113 @@ server.Get(R"(/(\d+)/reports)", [&](const httplib::Request& req, httplib::Respon
         }
 
         submission["reports"].push_back(body);
-
+        submission["edited_at"].push_back(date);
         std::ofstream save_file(PATH);
         save_file << submission.dump(4);
-        if (Submission.count(submission_id)) res.set_content("Submission Already Existed!", "text/plain");
-        if (Report.count(body["report_id"])) res.set_content("Report_Id Already Existed!", "text/plain");
+        if (Submission.count(submission_id) || Report.count(report_id)) {
+            res.status = 404;
+            res.set_content("Submission Or Report_Id Already Existed!", "text/plain");
+            return;
+        }
         Submission.insert(submission_id);
         Report.insert(report_id);
         res.set_content(submission.dump(4), "application/json");
     });
+
+
+    /*PATCH*/
+
+    server.Patch(R"(/(\d+)/reports/(\d+))", [&](const httplib::Request& req, httplib::Response& res){
+        json body = json::parse(req.body);
+        // WHITELIST KEYWORDS //
+        static const std::unordered_set<std::string> ALLOWED_EDITS = {
+            "link",
+            "status",
+            "description",
+            "credit"
+        };
+        /* KHONG CHO EDIT REPORT_ID */
+        if (body.contains("report_id")) {
+            res.status = 400;
+            res.set_content("report_id is immutable", "text/plain");
+            return;
+        }
+
+        /* Check Keywords */
+        for(auto& item : body.items()) {
+            const std::string& key = item.key();
+            if (!ALLOWED_EDITS.count(key)) {
+                res.status = 404;
+                res.set_content("field " + key + "IS NOT ALLOWED TO EDIT", "text/plain");
+                return;
+            }
+        }
+
+        int submission_id = std::stoi(req.matches[1]);
+        int report_id = std::stoi(req.matches[2]);
+        std::string PATH = "data/" + std::to_string(submission_id) + ".json";
+        json submission;
+        std::ifstream file(PATH);
+        if (!file.is_open()) {
+            res.status = 404;
+            res.set_content("File Not Found",  "text/plain");
+            return;
+        }
+        file >> submission;
+        bool found = false;
+        auto& submission_reports = submission["reports"];
+        for (auto& reports : submission_reports) {
+            if (reports["report_id"] == report_id) {
+                reports.merge_patch(body);
+                found = true;
+            }
+        }
+        if (!found) {
+            res.status = 404;
+            res.set_content("Invalid & Report_ID Not Existed", "text/plain");
+            return;
+        }
+        std::ofstream output_file(PATH);
+        output_file << submission.dump(4);
+        res.set_content(submission.dump(4), "application/json");
+    });
+
+    /*DELETE*/
+    server.Delete(R"(/(\d+)/reports/(\d+))", [&](const httplib::Request& req, httplib::Response& res){
+        int submission_id = std::stoi(req.matches[1]);
+        int report_id = std::stoi(req.matches[2]);
+        std::string PATH = "data/" + std::to_string(submission_id) + ".json";
+
+        /*Read File*/
+        std::ifstream file(PATH);
+        if (!file.is_open()) {
+            res.status = 404;
+            res.set_content("File NOT FOUND", "text/plain");
+            return;
+        }
+        json submission;
+        file >> submission;
+        bool found = false;
+        auto& Report_IDs = submission["reports"];
+        for (size_t i = 0; i < Report_IDs.size(); i++) {
+            if (Report_IDs[i]["report_id"] == report_id) {
+                Report_IDs.erase(Report_IDs.begin() + i);
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            res.status = 404;
+            res.set_content("ReportID NOT FOUND", "text/plain");
+            return;
+        }
+
+        std::ofstream Output_File(PATH);
+        Output_File << submission.dump(4);
+        res.set_content(submission.dump(4), "application/json");
+    });
+
+    /* LISTENING TO SERVERS */
         std::cout << "server on http://127.0.0.1:8000\n";
         server.listen("127.0.0.1", 8000);
     return 0;
